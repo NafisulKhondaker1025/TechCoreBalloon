097a3cb7623af731097bef544bb1c2c9afd957aa
Nafisul khondaker <nafisulkhondaker@email.arizona.edu>
2021-12-07 10:31:24 -0700

    test

--- js/changeAnimation.js
+++ js/changeAnimation.js
@@ -5,7 +5,7 @@ AFRAME.registerComponent('next-button', {
 
 
       const nextButton = document.getElementById('nextbutton')
-      //nextButton.style.display = 'block'
+      nextButton.style.display = 'block'
       // Start with the 2nd animation because the model starts with idle animation
       const nextAnimation = () => {
         
